72fd7c4ba457281f689d7338d3483d1a5a49a171
Ramu Gopal <190281896+ramugopal92@users.noreply.github.com>
2026-03-12 23:19:33 +0530

    Add files via upload

---
+++ FrmBendCalc.Designer.vb
@@ -0,0 +1,296 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FrmBendCalc
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(FrmBendCalc))
+        Me.txtThickness = New System.Windows.Forms.TextBox()
+        Me.txtRadius = New System.Windows.Forms.TextBox()
+        Me.txtAngle = New System.Windows.Forms.TextBox()
+        Me.txtK = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtsetback = New System.Windows.Forms.TextBox()
+        Me.txtBA = New System.Windows.Forms.TextBox()
+        Me.txtBD = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'txtThickness
+        '
+        Me.txtThickness.AccessibleName = "txtThickness"
+        Me.txtThickness.Location = New System.Drawing.Point(160, 16)
+        Me.txtThickness.Name = "txtThickness"
+        Me.txtThickness.Size = New System.Drawing.Size(150, 20)
+        Me.txtThickness.TabIndex = 0
+        '
+        'txtRadius
+        '
+        Me.txtRadius.AccessibleName = "txtRadius"
+        Me.txtRadius.Location = New System.Drawing.Point(160, 51)
+        Me.txtRadius.Name = "txtRadius"
+        Me.txtRadius.Size = New System.Drawing.Size(150, 20)
+        Me.txtRadius.TabIndex = 0
+        '
+        'txtAngle
+        '
+        Me.txtAngle.AccessibleName = "txtAngle"
+        Me.txtAngle.Location = New System.Drawing.Point(160, 88)
+        Me.txtAngle.Name = "txtAngle"
+        Me.txtAngle.Size = New System.Drawing.Size(150, 20)
+        Me.txtAngle.TabIndex = 0
+        '
+        'txtK
+        '
+        Me.txtK.AccessibleName = "txtK"
+        Me.txtK.Location = New System.Drawing.Point(160, 124)
+        Me.txtK.Name = "txtK"
+        Me.txtK.Size = New System.Drawing.Size(150, 20)
+        Me.txtK.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.White
+        Me.Label1.Location = New System.Drawing.Point(15, 16)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(142, 15)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Sheet Thickness (in):"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.White
+        Me.Label2.Location = New System.Drawing.Point(15, 51)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(119, 15)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Bend Radius (in):"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.Color.White
+        Me.Label3.Location = New System.Drawing.Point(15, 88)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(104, 15)
+        Me.Label3.TabIndex = 1
+        Me.Label3.Text = "Bend Angle (°):"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.ForeColor = System.Drawing.Color.White
+        Me.Label4.Location = New System.Drawing.Point(15, 124)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(65, 15)
+        Me.Label4.TabIndex = 1
+        Me.Label4.Text = "K-Factor:"
+        '
+        'txtsetback
+        '
+        Me.txtsetback.AccessibleName = "txtsetback"
+        Me.txtsetback.Enabled = False
+        Me.txtsetback.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtsetback.Location = New System.Drawing.Point(160, 183)
+        Me.txtsetback.Name = "txtsetback"
+        Me.txtsetback.Size = New System.Drawing.Size(150, 21)
+        Me.txtsetback.TabIndex = 0
+        '
+        'txtBA
+        '
+        Me.txtBA.AccessibleName = "txtBA"
+        Me.txtBA.Enabled = False
+        Me.txtBA.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtBA.Location = New System.Drawing.Point(160, 218)
+        Me.txtBA.Name = "txtBA"
+        Me.txtBA.Size = New System.Drawing.Size(150, 21)
+        Me.txtBA.TabIndex = 0
+        '
+        'txtBD
+        '
+        Me.txtBD.AccessibleName = "txtBD"
+        Me.txtBD.Enabled = False
+        Me.txtBD.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtBD.Location = New System.Drawing.Point(160, 255)
+        Me.txtBD.Name = "txtBD"
+        Me.txtBD.Size = New System.Drawing.Size(150, 21)
+        Me.txtBD.TabIndex = 0
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.ForeColor = System.Drawing.Color.White
+        Me.Label5.Location = New System.Drawing.Point(15, 183)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(88, 15)
+        Me.Label5.TabIndex = 1
+        Me.Label5.Text = "Setback (in):"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.ForeColor = System.Drawing.Color.White
+        Me.Label6.Location = New System.Drawing.Point(15, 218)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(139, 15)
+        Me.Label6.TabIndex = 1
+        Me.Label6.Text = "Bend Allowance (in):"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.ForeColor = System.Drawing.Color.White
+        Me.Label7.Location = New System.Drawing.Point(15, 255)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(139, 15)
+        Me.Label7.TabIndex = 1
+        Me.Label7.Text = "Bend Deduction (in):"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(335, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(329, 272)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 2
+        Me.PictureBox1.TabStop = False
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(8, 148)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(0, 13)
+        Me.Label8.TabIndex = 3
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.ForeColor = System.Drawing.SystemColors.ButtonShadow
+        Me.Label9.Location = New System.Drawing.Point(4, 153)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(325, 13)
+        Me.Label9.TabIndex = 4
+        Me.Label9.Text = "---------------------------------------------------------------------------------" &
+    "-------------------------"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.BackColor = System.Drawing.Color.White
+        Me.Label10.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label10.ForeColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.Label10.Location = New System.Drawing.Point(561, 266)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(98, 13)
+        Me.Label10.TabIndex = 5
+        Me.Label10.Text = "thetechthinker.com"
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.BackColor = System.Drawing.Color.White
+        Me.Label11.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.5!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label11.ForeColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.Label11.Location = New System.Drawing.Point(545, 264)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(18, 17)
+        Me.Label11.TabIndex = 6
+        Me.Label11.Text = "©"
+        '
+        'FrmBendCalc
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.MenuHighlight
+        Me.ClientSize = New System.Drawing.Size(676, 296)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtBD)
+        Me.Controls.Add(Me.txtK)
+        Me.Controls.Add(Me.txtBA)
+        Me.Controls.Add(Me.txtAngle)
+        Me.Controls.Add(Me.txtsetback)
+        Me.Controls.Add(Me.txtRadius)
+        Me.Controls.Add(Me.txtThickness)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "FrmBendCalc"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Bend Allowance Calculator -  v.1.0"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtThickness As TextBox
+    Friend WithEvents txtRadius As TextBox
+    Friend WithEvents txtAngle As TextBox
+    Friend WithEvents txtK As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txtsetback As TextBox
+    Friend WithEvents txtBA As TextBox
+    Friend WithEvents txtBD As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label11 As Label
+End Class
---
+++ FrmBendCalc.vb
@@ -0,0 +1,136 @@
+﻿Public Class FrmBendCalc
+
+    Private validationShown As Boolean = False
+
+    Private Sub FrmBendCalc_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Clear outputs when form starts
+        ClearOutputs()
+    End Sub
+
+    '==========================
+    ' On-type triggers
+    '==========================
+    Private Sub txtThickness_TextChanged(sender As Object, e As EventArgs) Handles txtThickness.TextChanged
+        CalculateValues()
+    End Sub
+
+    Private Sub txtRadius_TextChanged(sender As Object, e As EventArgs) Handles txtRadius.TextChanged
+        CalculateValues()
+    End Sub
+
+    Private Sub txtAngle_TextChanged(sender As Object, e As EventArgs) Handles txtAngle.TextChanged
+        CalculateValues()
+    End Sub
+
+    Private Sub txtK_TextChanged(sender As Object, e As EventArgs) Handles txtK.TextChanged
+        CalculateValues()
+    End Sub
+
+    Private Sub NumericTextBox_KeyPress(sender As Object, e As KeyPressEventArgs) _
+        Handles txtThickness.KeyPress, txtRadius.KeyPress, txtAngle.KeyPress, txtK.KeyPress
+
+        Dim tb As TextBox = DirectCast(sender, TextBox)
+
+        ' Allow control keys like Backspace
+        If Char.IsControl(e.KeyChar) Then Exit Sub
+
+        ' Allow digits
+        If Char.IsDigit(e.KeyChar) Then Exit Sub
+
+        ' Allow one decimal point
+        If e.KeyChar = "."c AndAlso Not tb.Text.Contains(".") Then Exit Sub
+
+        ' Block everything else
+        e.Handled = True
+    End Sub
+
+    '==========================
+    ' Main calculation routine
+    '==========================
+    Private Sub CalculateValues()
+        Dim T As Double, R As Double, A As Double, K As Double
+        Dim BA As Double, SB As Double, BD As Double
+        Dim PI As Double = Math.PI
+
+        ' If any input is blank or non-numeric → clear output and exit silently
+        If Not GetNumber(txtThickness, T) Then ClearOutputs() : Exit Sub
+        If Not GetNumber(txtRadius, R) Then ClearOutputs() : Exit Sub
+        If Not GetNumber(txtAngle, A) Then ClearOutputs() : Exit Sub
+        If Not GetNumber(txtK, K) Then ClearOutputs() : Exit Sub
+
+        ' Engineering validation
+        If T <= 0 Then
+            ClearOutputs()
+            ShowValidationMessage("Sheet Thickness must be greater than zero.", txtThickness)
+            Exit Sub
+        End If
+
+        If R <= 0 Then
+            ClearOutputs()
+            ShowValidationMessage("Bend Radius must be greater than zero.", txtRadius)
+            Exit Sub
+        End If
+
+        If A <= 0 Or A > 180 Then
+            ClearOutputs()
+            ShowValidationMessage("Bend Angle must be between 0 and 180 degrees.", txtAngle)
+            Exit Sub
+        End If
+
+        If K <= 0 Or K > 0.5 Then
+            ClearOutputs()
+            ShowValidationMessage("K-Factor must be between 0 and 0.5.", txtK)
+            Exit Sub
+        End If
+
+        '=== FORMULAS ===
+        Dim Arad As Double = A * PI / 180.0
+
+        ' Setback
+        SB = (R + T) * Math.Tan((A / 2.0) * PI / 180.0)
+
+        ' Bend Allowance
+        BA = Arad * (R + K * T)
+
+        ' Bend Deduction
+        BD = 2.0 * SB - BA
+
+        '=== DISPLAY ===
+        txtsetback.Text = FixZero(SB).ToString("0.000")
+        txtBA.Text = FixZero(BA).ToString("0.000")
+        txtBD.Text = FixZero(BD).ToString("0.000")
+    End Sub
+
+    '==========================
+    ' Helpers
+    '==========================
+    Private Function GetNumber(tb As TextBox, ByRef outVal As Double) As Boolean
+        Dim s As String = Trim(tb.Text)
+
+        If s = "" Then Return False
+
+        Return Double.TryParse(s, outVal)
+    End Function
+
+    Private Sub ClearOutputs()
+        txtsetback.Text = ""
+        txtBA.Text = ""
+        txtBD.Text = ""
+    End Sub
+
+    Private Function FixZero(v As Double) As Double
+        If Math.Abs(v) < 0.0000001 Then v = 0
+        Return v
+    End Function
+
+    Private Sub ShowValidationMessage(msg As String, tb As TextBox)
+        If validationShown Then Exit Sub
+
+        validationShown = True
+        MessageBox.Show(msg, "Bend Allowance Calculator", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+        tb.Focus()
+        tb.SelectAll()
+        validationShown = False
+    End Sub
+
+End Class
